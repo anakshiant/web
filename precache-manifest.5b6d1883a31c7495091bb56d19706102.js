d86fad6e27cc3b898bfc161bee21b8d1fe0923f1
anakshiant <nndchaudhary8@gmail.com>
2020-04-23 01:27:59 +0530

    local

---
+++ precache-manifest.5b6d1883a31c7495091bb56d19706102.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de8f2751eb4d87be30200b6beff86d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "792ca2694ded0cb0045f",
+    "url": "/static/css/main.6375d7fe.chunk.css"
+  },
+  {
+    "revision": "7e97ffa914d6edfb6ec5",
+    "url": "/static/js/2.f5a7de4a.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.f5a7de4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "792ca2694ded0cb0045f",
+    "url": "/static/js/main.db4ef035.chunk.js"
+  },
+  {
+    "revision": "20742fbae1f3e16c1374",
+    "url": "/static/js/runtime-main.93f688b1.js"
+  }
+]);
